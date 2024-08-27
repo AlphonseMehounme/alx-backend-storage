62d936b953f856031b776a863f03138846340100
Alphonse <alphonsemehounme9@gmail.com>
2024-08-27 08:28:49 +0000

    add function Safediv

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,13 @@
+-- 10-div.sql
+-- create SafeDiv function
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT
+BEGIN
+	IF b = 0 THEN
+		RETURN 0;
+	ELSE
+		RETURN (a / b);
+	END IF;
+END$$
+DELIMITER ;
