c666fbe35aca6d52b2d857433140ad09f7a927de
Alphonse Mehounme <alphonsemehounme9@gmail.com>
2024-08-29 05:24:52 -0700

    find with regex

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
     ng = client.logs.nginx
     print(f"{ng.count_documents({})} logs")
-    print("Methods")
+    print("Methods:")
     for met in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         print("\tmethod {}: {}".format(met, ng.count_documents({ "method": met })))
     print("{} status check".format(ng.count_documents({ "$and": [ {"method": "GET"}, { "path": "/status" } ] })))
