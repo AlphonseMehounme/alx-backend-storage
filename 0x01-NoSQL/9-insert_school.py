61c1cc4a685d45b83f66d2520ee7a749f23b67f8
Alphonse Mehounme <alphonsemehounme9@gmail.com>
2024-08-29 02:40:04 -0700

    insert doc

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Insert new doc
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    add new doc to mong_collection
+    """
+    doc = {}
+    for key, val in kwargs.items():
+        doc.update({key: val})
+    mongo_collection.insert_one(doc)
